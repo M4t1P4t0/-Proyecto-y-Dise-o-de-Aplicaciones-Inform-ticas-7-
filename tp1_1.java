097a7568d9777e9cc91707f8ce8b574a3efe8ff6
M4t1P4t0 <129062009+M4t1P4t0@users.noreply.github.com>
2024-03-25 14:28:43 -0300

    Matis Gisler Tp1.1

---
+++ tp1_1.java
@@ -0,0 +1,15 @@
+package mati;
+import java.util.Scanner;
+public class tp1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int numero=0;
+		System.out.println("Ingrese un numero");
+		numero= sc.nextInt();
+		System.out.println("Tu numero es "+numero);
+	}
+
+}
